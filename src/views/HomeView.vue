471a9c8886118c53e2ccbea0d27f5756ac46a0cf
aikhe <ikeandrie.ro@gmail.com>
2025-09-13 21:00:22 +0800

    feat(waneyy): initial work-in-progress homepage + clean-up routes & styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,108 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink } from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const name = ref('Unknown')
+
+const getName = async () => {
+  const res = await fetch('/api/')
+  const data = await res.json()
+  name.value = data.name
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section class="container">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <button class="" @click="getName" aria-label="get name">Name from API is: {{ name }}</button>
+      <p>Edit <code>server/index.ts</code> to change what the API gets</p>
+      <nav>
+        <RouterLink to="/">Waneyy</RouterLink>
+        <RouterLink to="/home">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </section>
 </template>
+
+<style>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+  padding: 3px;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+button {
+  background-color: hsla(160, 100%, 37%, 1);
+  color: var(--color-background);
+  border: 0;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  margin: 1rem 0 0.5rem 0;
+}
+
+.container {
+  display: flex;
+  place-items: center;
+  padding-right: calc(var(--section-gap) / 2);
+}
+
+.logo {
+  margin: 0 2rem 0 0;
+}
+
+header .wrapper {
+  display: flex;
+  place-items: flex-start;
+  flex-wrap: wrap;
+}
+
+nav {
+  text-align: left;
+  margin-left: -1rem;
+  font-size: 1rem;
+
+  margin-top: 1rem;
+}
+</style>
---
+++ src/views/WaneyyView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <p class="">Work in Progress...</p>
+  </section>
+</template>
